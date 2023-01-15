b91265e014093f378ee9bf7738b757f415218b06
Kshitiz <84060730+KshitizTrashh@users.noreply.github.com>
2023-01-15 20:14:53 +0545

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+app.get('/file', (req, res) => {
+  fs.readFile('file.txt', 'utf8', (err, data) => {
+    if (err) return res.status(500).send(err);
+    res.send(`<div id="file-contents">${data}</div>`);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
